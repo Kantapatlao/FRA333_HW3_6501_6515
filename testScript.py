67cee539b03bfa317829895197d389193a5b4e88
Kantapatlao <kantapatlao2004@gmail.com>
2024-10-12 19:00:45 +0700

    Finished_Proof2

--- testScript.py
+++ testScript.py
@@ -9,7 +9,12 @@ import HW3_utils as utils
 import roboticstoolbox as rtb
 import numpy as np
 import FRA333_HW3_6501_6515 as Rob
-
+d_1 = 0.0892
+a_2 = -0.425
+a_3 = -0.39243
+d_4 = 0.109
+d_5 = 0.093
+d_6 = 0.082
 
 from spatialmath import SE3
 from spatialmath.base import *
@@ -17,28 +22,59 @@ from math import pi
 import math
 robot = rtb.DHRobot(
     [
-        rtb.RevoluteMDH(a = 0, alpha = 0, d = utils.d_1, offset = pi),
+        rtb.RevoluteMDH(a = 0, alpha = 0, d = d_1, offset = pi),
         rtb.RevoluteMDH(a = 0, alpha = pi, d = 0, offset = 0),
-        rtb.RevoluteMDH(a = 0, alpha = utils.a_2, d = 0, offset = 0)
+        rtb.RevoluteMDH(a = 0, alpha = a_2, d = 0, offset = 0)
     ], name="HW3rob")
+
 #create joint3 to end-effect
-translate_to_end = SE3(utils.a_3 + (-utils.d_6),-utils.d_5,utils.d_4)
+translate_to_end = SE3(a_3 + (-d_6),-d_5,d_4)
 #add end-effect to robot
 robot.tool = translate_to_end
-print(robot)
 #===========================================<ตรวจคำตอบข้อ 1>====================================================#
 #code here
 
 
 def Proof1(q:list[float])->list[float]:
     # find jacobian from frame 0
+
     J = robot.jacob0(q) 
+    print("------------------Jacobian------------------------")
+    J_linear = J[:3]
+    J_angular = J[3:]
+    print(J_linear)
+    print(J_angular)
 #==============================================================================================================#
 #===========================================<ตรวจคำตอบข้อ 2>====================================================#
 #code here
+def Proof2(q:list[float]):
+    # find jacobian from frame 0
+    J = robot.jacob0(q) 
+    J_reduce = J[:3,:] #:3 เอาเเถวบนถึงเเถวที่ 3 , : เท่าหลักทุกตัว
+    J_det = np.linalg.det(J_reduce)  #เช็คค่าsingular ต่อไปคือถ้าค่าใกล้ 0 จะถือว่า det เป็น 0 ก็คือการเป็น singularity
+    if abs(J_det) <= 0.00001: #เนื่องจาก det ได้ค่าออกเเค่ตัวเดียว norm คือการรวมค่ามาเป้น vector เดียว เลยต้องใช้เป้น abs เเทน
+        kebka_sing = True
+    else:
+        kebka_sing = False
+
+    print("-----------Singularity-------------------")
+    print(kebka_sing)
+    print(J_det)
+    
+
+
+
+
 
 #==============================================================================================================#
 #===========================================<ตรวจคำตอบข้อ 3>====================================================#
 #code here
 
 #==============================================================================================================#
+
+#=======================================Test Zone=======================================================#
+#param
+# q = Rob.q  รอดึง q จากไฟล์จู
+q = [1,2,3]
+Proof1(q)
+Proof2(q)
